e00f6aa4dd26ae75c9c6cc39c3a4fa6b1d6e693b
sander <sander.sohngen@usp.br>
2022-11-30 19:26:18 -0300

    corrected pandas' plot

--- learn_pandas.py
+++ learn_pandas.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-db = pd.read_csv("BancoDados.csv", parse_dates=True, index_col=2)
-# teste = db[["Coluna", "Data"]]
-db.plot()
+db = pd.read_csv("BancoDados.csv", parse_dates=True, index_col=2)["Coluna"]
+db.plot.bar()
 plt.show()
